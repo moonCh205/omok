ca5f807e9e3d27a29982900a042f05294deb559e
정준영 <fdjy0101@gmail.com>
2023-05-06 03:59:38 +0900

    커밋 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Game from './components/Game'
+import Game from './components/Game';
+
 function App() {
   return (
-    <div className="App">
-     <Game/>
+    <div className='App'>
+      <Game />
     </div>
   );
 }
